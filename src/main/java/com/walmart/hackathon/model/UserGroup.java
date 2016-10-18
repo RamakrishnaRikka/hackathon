b1b98a5889289e1d1e65de5d111ab0756a5a9c79
Roshan R <roshan.atl@gmail.com>
2016-10-18 15:41:29 +0530

    Update UserGroup.java

--- src/main/java/com/walmart/hackathon/model/UserGroup.java
+++ src/main/java/com/walmart/hackathon/model/UserGroup.java
@@ -1,13 +1,17 @@
 package com.walmart.hackathon.model;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class UserGroup {
@@ -24,6 +28,18 @@ public class UserGroup {
 	@NotNull
 	@Column(name = "created_by")
 	BigInteger createdBy;
+	
+	@JsonProperty
+	@Transient
+	List<HackUser> members;
+
+	public List<HackUser> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<HackUser> members) {
+		this.members = members;
+	}
 
 	public BigInteger getGroupId() {
 		return groupId;

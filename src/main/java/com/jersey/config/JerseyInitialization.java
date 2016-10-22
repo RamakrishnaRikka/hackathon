04dd6a6b1b4ec713635c06dedf18076ccfd583b9
sashikantamalik <sashikantamallick911@gmail.com>
2016-10-22 17:14:23 +0530

    Update JerseyInitialization.java

--- src/main/java/com/jersey/config/JerseyInitialization.java
+++ src/main/java/com/jersey/config/JerseyInitialization.java
@@ -4,6 +4,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.walmart.hackathon.resources.ItemResource;
+import com.walmart.hackathon.resources.UserResource;
 
 public class JerseyInitialization extends ResourceConfig {
     /**
@@ -13,11 +15,9 @@ public class JerseyInitialization extends ResourceConfig {
         this.register(new JacksonJsonProvider(ObjectMapperFactory.create()));
         this.property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
         this.property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
-        //this.packages(true, "com.jersey.resources");
-        this.packages(true, "com.walmart.hackathon.resources");
-        /*this.register(UserResource.class);
-        this.register(GroupResource.class);
-        this.register(BucketListResource.class);
-        this.register(ItemResource.class);*/
+        
+        this.register(UserResource.class);
+        this.register(ItemResource.class);
     }
 }
+

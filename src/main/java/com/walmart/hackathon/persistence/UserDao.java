2a02616acb48bfc7867a10dea554ccff7eb1f781
sashikantamalik <sashikantamallick911@gmail.com>
2016-10-25 11:17:28 +0530

    Update UserDao.java

--- src/main/java/com/walmart/hackathon/persistence/UserDao.java
+++ src/main/java/com/walmart/hackathon/persistence/UserDao.java
@@ -3,9 +3,12 @@ package com.walmart.hackathon.persistence;
 import java.math.BigInteger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.walmart.hackathon.model.HackUser;
 
 public interface UserDao extends JpaRepository<HackUser,BigInteger>{
+	@Query("select u from HackUser u where u.email=?1")
+	public HackUser findUsersByEmail(String  email);
 
 }
